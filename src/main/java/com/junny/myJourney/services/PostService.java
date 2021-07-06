6e364465b37a1858082a0362102ca6b256fedd93
Junny Lee <junny1997@gmail.com>
2021-07-06 10:27:48 -0700

    clean up comments

--- src/main/java/com/junny/myJourney/services/PostService.java
+++ src/main/java/com/junny/myJourney/services/PostService.java
@@ -71,9 +71,6 @@ public class PostService {
 	 
 	 // created this one for DESC
 	 public List<Post> allPostsDesc(){
-//		 for (int i  = 0; i < postRepository.postsDescByDate().size(); i++) {
-//			 System.out.println(postRepository.postsDescByDate().get(i).getCreatedAt());
-//		 }
 		 return postRepository.postsDescByDate();
 	 }
 	 
